e77d6467c08f10b5a29f592276b6f914bcc76ed2
Admin <Admin@TEB123001>
2023-09-08 14:48:04 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("ERROR ERROR ERROR");
+            Console.WriteLine("PC NOT WORKING CORRECTLY");
+            Console.ReadKey();  
+           
+        }
+    }
+}
+
+        
